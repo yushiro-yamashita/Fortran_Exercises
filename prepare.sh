b7142688fcc76cf89f516f4af219efd356caf4e6
Yushiro Yamashita <yamashita.yuushirou.33c@st.kyoto-u.ac.jp>
2023-07-09 14:11:03 +0000

    update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,9 @@ dpkg --status liblapack-dev | grep -q not-installed
 if [ $? -eq 0 ]; then
     sudo apt install libblas-dev liblapack-dev
 fi
+if [[ ! $(pip list|grep "pandas") ]]; then
+    pip install pandas --quiet
+fi
 if [[ ! $(pip list|grep "sympy-plot-backends") ]]; then
-    pip install sympy_plot_backends
+    pip install sympy_plot_backends --quiet
 fi
